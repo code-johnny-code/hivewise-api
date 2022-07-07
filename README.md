201ecc658ef9381504eb8ccaa8348313bf70cf34
code-johnny-code <john.ross86@live.com>
2022-07-07 18:47:40 -0500

    updates

---
+++ README.md
@@ -0,0 +1,1 @@
+Based on https://github.com/mongodb-developer/mongodb-express-rest-api-example
--- server/routes/swarms.js
+++ server/routes/swarms.js
@@ -14,10 +14,10 @@ const h3 = require("h3-js");
 const hexResolution = 7;
 
 // This section will help you get a list of all the records.
-recordRoutes.route('/hives').get(async function (_req, res) {
+recordRoutes.route('/swarms').get(async function (_req, res) {
   const dbConnect = dbo.getDb();
   dbConnect
-    .collection('hives')
+    .collection('swarms')
     .find({})
     .limit(50)
     .toArray(function (err, result) {
@@ -30,26 +30,23 @@ recordRoutes.route('/hives').get(async function (_req, res) {
 });
 
 // This section will help you create a new record.
-recordRoutes.route('/hives').post(function (req, res) {
+recordRoutes.route('/swarms').post(function (req, res) {
   const dbConnect = dbo.getDb();
-  const {lat, lon} = req.body.hiveLoc;
-  const hiveDoc = {
-    hiveId: uuidv4(),
+  const {lat, lon, z} = req.body.swarmLoc;
+  const swarmDoc = {
+    swarmId: uuidv4(),
     created: new Date(),
-    hiveName: req.body.hiveName,
     hiveLoc: {
       lat: lat,
       lon: lon,
-      h3: {
-        "res": hexResolution,
-        "hex": h3.geoToH3(lat, lon, hexResolution)
-      }
+      z: z,
+      h3: h3.geoToH3(lat, lon, hexResolution)
     }
   }
 
   dbConnect
-    .collection('hives')
-    .insertOne(hiveDoc, function (err, result) {
+    .collection('swarms')
+    .insertOne(swarmDoc, function (err, result) {
       if (err) {
         res.status(400).send('Error inserting hive!');
       } else {
@@ -60,35 +57,35 @@ recordRoutes.route('/hives').post(function (req, res) {
 });
 
 // This section will help you update a record by id.
-recordRoutes.route('/listings/updateLike').post(function (req, res) {
-  const dbConnect = dbo.getDb();
-  const listingQuery = { _id: req.body.id };
-  const updates = {
-    $inc: {
-      likes: 1,
-    },
-  };
-
-  dbConnect
-    .collection('listingsAndReviews')
-    .updateOne(listingQuery, updates, function (err, _result) {
-      if (err) {
-        res
-          .status(400)
-          .send(`Error updating likes on listing with id ${listingQuery.id}!`);
-      } else {
-        console.log('1 document updated');
-      }
-    });
-});
+// recordRoutes.route('/listings/updateLike').post(function (req, res) {
+//   const dbConnect = dbo.getDb();
+//   const listingQuery = { _id: req.body.id };
+//   const updates = {
+//     $inc: {
+//       likes: 1,
+//     },
+//   };
+//
+//   dbConnect
+//     .collection('listingsAndReviews')
+//     .updateOne(listingQuery, updates, function (err, _result) {
+//       if (err) {
+//         res
+//           .status(400)
+//           .send(`Error updating likes on listing with id ${listingQuery.id}!`);
+//       } else {
+//         console.log('1 document updated');
+//       }
+//     });
+// });
 
 // This section will help you delete a record.
-recordRoutes.route('/hives').delete((req, res) => {
+recordRoutes.route('/swarms').delete((req, res) => {
   const dbConnect = dbo.getDb();
-  const hiveQuery = { hiveId: req.body.hiveId };
+  const swarmQuery = { swarmId: req.body.swarmId };
   dbConnect
-    .collection('hives')
-    .deleteOne(hiveQuery, function (err, _result) {
+    .collection('swarms')
+    .deleteOne(swarmQuery, function (err, _result) {
       if (err) {
         res
           .status(400)
