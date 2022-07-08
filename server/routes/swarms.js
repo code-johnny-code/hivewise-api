e1a6916b8dc8a77b1429504022570c8a37cc5b96
code-johnny-code <john.ross86@live.com>
2022-07-07 20:50:43 -0500

    cleaned up swarms route, differentiated between document deleted and document not found on delete

--- server/routes/swarms.js
+++ server/routes/swarms.js
@@ -1,21 +1,15 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
-
-// recordRoutes is an instance of the express router.
-// We use it to define our routes.
-// The router will be added as a middleware and will take control of requests starting with path /listings.
 const recordRoutes = express.Router();
-
-// This will help us connect to the database
 const dbo = require('../db/conn');
 
 // H3 configuration
 const h3 = require("h3-js");
 const hexResolution = 7;
 
-// This section will help you get a list of all the records.
 recordRoutes.route('/swarms').get(async function (_req, res) {
   const dbConnect = dbo.getDb();
+
   dbConnect
     .collection('swarms')
     .find({})
@@ -29,7 +23,6 @@ recordRoutes.route('/swarms').get(async function (_req, res) {
     });
 });
 
-// This section will help you create a new record.
 recordRoutes.route('/swarms').post(function (req, res) {
   const dbConnect = dbo.getDb();
   const {lat, lon, z} = req.body.swarmLoc;
@@ -56,42 +49,23 @@ recordRoutes.route('/swarms').post(function (req, res) {
     });
 });
 
-// This section will help you update a record by id.
-// recordRoutes.route('/listings/updateLike').post(function (req, res) {
-//   const dbConnect = dbo.getDb();
-//   const listingQuery = { _id: req.body.id };
-//   const updates = {
-//     $inc: {
-//       likes: 1,
-//     },
-//   };
-//
-//   dbConnect
-//     .collection('listingsAndReviews')
-//     .updateOne(listingQuery, updates, function (err, _result) {
-//       if (err) {
-//         res
-//           .status(400)
-//           .send(`Error updating likes on listing with id ${listingQuery.id}!`);
-//       } else {
-//         console.log('1 document updated');
-//       }
-//     });
-// });
-
-// This section will help you delete a record.
 recordRoutes.route('/swarms').delete((req, res) => {
   const dbConnect = dbo.getDb();
   const swarmQuery = { swarmId: req.body.swarmId };
+
   dbConnect
     .collection('swarms')
     .deleteOne(swarmQuery, function (err, _result) {
       if (err) {
         res
           .status(400)
-          .send(`Error deleting listing with id ${listingQuery.listing_id}!`);
-      } else {
+          .send(`Error deleting listing with id ${swarmQuery.swarmId}!`);
+      }
+      else if (_result.result.n) {
         console.log('1 document deleted');
+        res.sendStatus(204);
+      } else {
+        res.sendStatus(410)
       }
     });
 });
