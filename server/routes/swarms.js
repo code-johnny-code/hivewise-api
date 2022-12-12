6e14422c87195756f27d593b8e8b8ce4471d6925
John Ross <john.ross86@live.com>
2022-12-11 21:28:55 -0600

    fixed postman collection, yet another status code

--- server/routes/swarms.js
+++ server/routes/swarms.js
@@ -4,7 +4,7 @@ const recordRoutes = express.Router();
 const dbo = require('../db/conn');
 
 // H3 configuration
-const h3 = require("h3-js");
+const h3 = require('h3-js');
 const hexResolution = 7;
 
 recordRoutes.route('/swarms').get(async function (_req, res) {
@@ -28,7 +28,7 @@ recordRoutes.route('/swarms/active').get(async function (_req, res) {
 
   dbConnect
     .collection('swarms')
-    .find({activeSwarm: true})
+    .find({ activeSwarm: true })
     .limit(50)
     .toArray(function (err, result) {
       if (err) {
@@ -41,10 +41,10 @@ recordRoutes.route('/swarms/active').get(async function (_req, res) {
 
 recordRoutes.route('/swarms/byHex/:hex').get(async function (_req, res) {
   const dbConnect = dbo.getDb();
-  const {hex} = _req.params;
+  const { hex } = _req.params;
   dbConnect
     .collection('swarms')
-    .find({"swarmLoc.h3": hex})
+    .find({ 'swarmLoc.h3': hex })
     .toArray(function (err, result) {
       if (err) {
         res.status(400).send('Error fetching swarms');
@@ -56,7 +56,7 @@ recordRoutes.route('/swarms/byHex/:hex').get(async function (_req, res) {
 
 recordRoutes.route('/swarms').post(function (req, res) {
   const dbConnect = dbo.getDb();
-  const {lat, lon, z} = req.body.swarmLoc;
+  const { lat, lon, z } = req.body.swarmLoc;
   const swarmId = uuidv4();
   const swarmDoc = {
     swarmId: swarmId,
@@ -67,61 +67,60 @@ recordRoutes.route('/swarms').post(function (req, res) {
       lat: lat,
       lon: lon,
       z: z,
-      h3: h3.geoToH3(lat, lon, hexResolution)
+      h3: h3.geoToH3(lat, lon, hexResolution),
     },
-    contact: req.body.contact
-  }
+    contact: req.body.contact,
+  };
 
-  dbConnect
-    .collection('swarms')
-    .insertOne(swarmDoc, function (err) {
-      if (err) {
-        res.status(400).send('Error inserting swarm');
-      } else {
-        console.log(`Added swarm ${swarmId}`);
-        res.status(204).send();
-      }
-    });
+  dbConnect.collection('swarms').insertOne(swarmDoc, function (err) {
+    if (err) {
+      res.status(400).send('Error inserting swarm');
+    } else {
+      console.log(`Added swarm ${swarmId}`);
+      res.status(204).send();
+    }
+  });
 });
 
 recordRoutes.route('/swarms/deactivate').put(function (req, res) {
   const dbConnect = dbo.getDb();
-  const {swarmId} = req.body;
+  const { swarmId } = req.body;
   const swarmDoc = {
     swarmId: swarmId,
   };
 
   dbConnect
     .collection('swarms')
-    .updateOne(swarmDoc, {$set: {activeSwarm: false, deactivated: new Date()}}, function (err) {
-      if (err) {
-        res.status(400).send('Error deactivating swarm');
-      } else {
-        console.log(`Deactivated swarm ${swarmId}`);
-        res.status(204).send();
+    .updateOne(
+      swarmDoc,
+      { $set: { activeSwarm: false, deactivated: new Date() } },
+      function (err) {
+        if (err) {
+          res.status(400).send('Error deactivating swarm');
+        } else {
+          console.log(`Deactivated swarm ${swarmId}`);
+          res.status(204).send();
+        }
       }
-    });
+    );
 });
 
 recordRoutes.route('/swarms').delete((req, res) => {
   const dbConnect = dbo.getDb();
   const swarmQuery = { swarmId: req.body.swarmId };
 
-  dbConnect
-    .collection('swarms')
-    .deleteOne(swarmQuery, function (err, _result) {
-      if (err) {
-        res
-          .status(400)
-          .send(`Error deleting listing with id ${swarmQuery.swarmId}!`);
-      }
-      else if (_result.result.n) {
-        console.log('Swarm document deleted');
-        res.sendStatus(204);
-      } else {
-        res.sendStatus(410)
-      }
-    });
+  dbConnect.collection('swarms').deleteOne(swarmQuery, function (err, _result) {
+    if (err) {
+      res
+        .status(400)
+        .send(`Error deleting listing with id ${swarmQuery.swarmId}!`);
+    } else if (_result.result.n) {
+      console.log('Swarm document deleted');
+      res.sendStatus(204);
+    } else {
+      res.sendStatus(404);
+    }
+  });
 });
 
 module.exports = recordRoutes;
